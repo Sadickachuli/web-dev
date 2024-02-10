a815b7cefae2e7a3e424be3bc167aa3caa72940d
Sadickachuli <m.achuli@alustudent.com>
2024-02-09 17:04:48 -0800

    add sounds

---
+++ simon Game/game.js
@@ -0,0 +1,37 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").on("click", function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour =  buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(userChosenColour);
+    
+}
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+$("#" + currentColour).addClass("pressed");
+
+setTimeout(function(){
+  $("#" + currentColour).removeClass("pressed");
+}, 100);
+
+}
+
